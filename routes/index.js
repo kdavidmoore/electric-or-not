0c0df9617cf18363964416657fd6d8f88f1b4028
Keith Moore <keith.m4@gmail.com>
2016-04-22 15:02:19 -0400

    added images

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var mongoClient = mongo.MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/electric';
+var db;
+var myCars = [];
+
+// create a connection with mongo
+mongoClient.connect(mongoUrl, function(error, database){
+	db = database;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	db.collection('cars').find({}).toArray(function(error, carResult){
+		// get a random number between 1 and 4
+		var getRandomImage = Math.floor(Math.random() * carResult.length);
+		res.render('index', { carObj: carResult[getRandomImage].imageSrc });
+	});
 });
 
 module.exports = router;
