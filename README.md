8ec1d6f7607abe353a261424b9f74200bcd8f35f
Keith Moore <keith.m4@gmail.com>
2016-04-26 14:09:45 -0400

    added standings page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Electric or Not
 
-### This app is built in Node.js using Express.js and also uses MongoDB. It shows the user an electric car, and the user votes on whether the car is "electric" or not. The votes are stored as a collections so any user can see all votes for any car.
+### This app is built in Node.js using Express.js and also uses MongoDB. It shows the user a car, and the user votes on whether the car is "electric" or not. The votes are stored as collections so any user can see the total score/rating for each car.
--- routes/index.js
+++ routes/index.js
@@ -24,28 +24,28 @@ router.get('/', function(req, res, next) {
 	// get the current user
 	var currIP = req.ip;
 	db.collection('users').find({ip: currIP}).toArray(function(error, userResult){
-		// make sure photosToShow is initialized
+		// initialize photosToShow (will stay this value unless there are vote records available)
 		photosToShow = allCars;
 		// just show the cars that don't have a totalVotes field
 		if (userResult.length > 0){
 			db.collection('cars').find({ totalVotes: { $exists: false } }).toArray(function(error, results){
-			console.log(results);
 				if (results.length > 0) {
+					// set the value of photosToShow to the current query results
 					photosToShow = results;
-					console.log('=======================');
-					console.log('showing partial results');
-					console.log('=======================');
+					console.log("==============");
+					console.log(results);
+					console.log("==============");
 					var randomPhoto = Math.floor(Math.random() * photosToShow.length);
 					res.render('index', { carName: photosToShow[randomPhoto].name, carImg: photosToShow[randomPhoto].src});
 				} else {
 					return "all photos have been voted on";
 				}
-			});
+			}); // end query to 'cars' collection
 		} else {
 			var randomPhoto = Math.floor(Math.random() * photosToShow.length);
 			res.render('index', { carName: photosToShow[randomPhoto].name, carImg: photosToShow[randomPhoto].src});
 		}
-	});
+	}); // end query to 'users' collection
 });
 
 
@@ -64,12 +64,12 @@ router.post('/electric', function(req, res, next){
 	db.collection('users').insert(
 		{
 			ip: req.ip,
-			"car": req.body.car,
-			"vote": "electric"
+			car: req.body.car,
+			vote: "electric"
 		}, function(error, results){console.log(error);}
 	);
 
-	// redirect to the home page when done updating tables
+	// redirect to the home page when done updating collections
 	res.redirect('/');
 });
 
@@ -80,7 +80,7 @@ router.post('/not', function(req, res, next){
 			name: req.body.car
 		},
 		{
-			$inc: {"totalVotes": 1}
+			$inc: {"totalVotes": -1}
 		}, function(error, results){console.log(error);}
 	);
 
@@ -88,13 +88,26 @@ router.post('/not', function(req, res, next){
 	db.collection('users').insert(
 		{
 			ip: req.ip,
-			"car": req.body.car,
-			"vote": "not"
+			car: req.body.car,
+			vote: "not"
 		}, function(error, results){console.log(error);}
 	);
 
-	// redirect to the home page when done updating tables
+	// redirect to the home page when done updating collections
 	res.redirect('/');
+});
+
+
+router.get('/standings', function(req, res, next){
+	// get all the photos
+	db.collection('cars').find().toArray(function(error, results){
+		// sort photos by total votes
+		results.sort(function(a, b){
+			return (b.totalVotes - a.totalVotes);
+		});
+		console.log(results);
+		res.render('standings', { standings: results });
+	}); // end query to 'cars' collection
 });
 
 module.exports = router;
